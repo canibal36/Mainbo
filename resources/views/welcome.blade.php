ad17aa8b6f081f4402687a64d012ccac4826ffdf
leonel <admin@leonel.inc>
2023-12-02 21:12:21 -0400

    cambio en los textos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1047,47 +1047,30 @@
                             <ul>
                                 <li><a href="contact.html"><span></span>Portal de ayuda</a></li>
                                 <li><a href="contact.html"><span></span>Contactenos</a></li>
-                                <li><a href="error.html"><span></span>Informacion de envio</a></li>
-                                <li><a href="error.html"><span></span>Entrega</a></li>
-                                <li><a href="error.html"><span></span>Reembolso o devoluciones</a></li>
+                                <li><a href="error.html"><span></span>Informacion de envio, Entrega y reembolso o devoluciones</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-duration="1.2s" data-wow-delay=".2s">
                         <div class="footer__item">
-                            <h4 class="footer-title">Get to Know Us</h4>
+                            <h4 class="footer-title">Infomacion sobre la tienda </h4>
                             <ul>
                                 <li><a href="about.html"><span></span>Sobre nosotros</a></li>
                                 <li><a href="blog-grid.html"><span></span>Informacion de Mainboo</a></li>
-                                <li><a href="error.html"><span></span>carreras</a></li>
-                                <li><a href="error.html"><span></span>Investors</a></li>
-                                <li><a href="contact.html"><span></span>Contact Us</a></li>
                             </ul>
                         </div>
                     </div>
-                    <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".3s">
-                        <div class="footer__item">
-                            <h4 class="footer-title">vapes new collections</h4>
-                            <ul>
-                                <li><a href="shop.html"><span></span>E-Cigarettes</a></li>
-                                <li><a href="shop.html"><span></span>Vape Pens</a></li>
-                                <li><a href="shop.html"><span></span>Pod Systems</a></li>
-                                <li><a href="shop.html"><span></span>Disposable Vapes</a></li>
-                                <li><a href="shop.html"><span></span>Nicotine Salt Devices</a></li>
-                            </ul>
-                        </div>
-                    </div>
+                    
                     <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-duration="1.4s" data-wow-delay=".4s">
                         <div class="footer__item newsletter">
-                            <h4 class="footer-title">get newsletter</h4>
+                            <h4 class="footer-title">Conocernos</h4>
                             <div class="subscribe">
-                                <input type="email" placeholder="Your Email">
+                                <input type="email" placeholder="Recibiras notificaciones de nuestras promociones">
                                 <button><i class="fa-solid fa-paper-plane"></i></button>
                             </div>
                             <div class="social-icon mt-40">
                                 <a href="#0"><i class="fa-brands fa-facebook-f"></i></a>
-                                <a href="#0"><i class="fa-brands fa-twitter"></i></a>
-                                <a href="#0"><i class="fa-brands fa-linkedin-in"></i></a>
+                                <a href="#0"><i class="fab fa-tiktok"></i></a>  
                                 <a href="#0"><i class="fa-brands fa-youtube"></i></a>
                             </div>
                         </div>
@@ -1098,7 +1081,7 @@
                 <a href="index.html" class="logo d-block">
                     <img src="assets-landing/images/logo/logo.svg" alt="logo">
                 </a>
-                <p>&copy; Copyright 2023 <a href="#0" class="primary-hover">odor</a> All Rights Reserved</p>
+                <p>&copy; Leonel.inc <a href="#0" class="primary-hover">Mainbo</a>2023</p>
                 <a href="#0" class="payment d-block image">
                     <img src="assets-landing/images/icon/payment.png" alt="icon">
                 </a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductoController;
+
+
 
 /*
 |--------------------------------------------------------------------------
