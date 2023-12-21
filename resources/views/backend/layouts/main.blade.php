f4f05f6755a1a887c531df51831d1d0705995ac4
ynvYauneEnovore <yovanuxf@gmail.com>
2023-12-21 17:48:57 -0400

    reestrucutra de das

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es-BO" dir="ltr" class="light">
 
 <head>
-
     @include('backend.layouts.meta')
     @include('backend.layouts.styles')
-
 </head>
 
-<body>
+<body class=" font-inter dashcode-app" id="body_class">
+    <main class="app-wrapper">
+        @include('backend.layouts.header')
+        @include('backend.layouts.menu')
+        @yield('content')
+        @include('backend.layouts.footer')
+        @include('backend.layouts.scripts')
 
-
-    @yield('content')
-
-    @include('backend.layouts.scripts')
-
-
+    </main>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,5 @@ Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('backend.dashboard.index');
 });
