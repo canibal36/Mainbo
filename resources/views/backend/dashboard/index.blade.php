4bdfe3bc42aec4cef98700165ce11de136ef3928
YovanEnovore <yovanuxf@gmail.com>
2023-12-31 12:26:42 -0400

    sad

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layouts.main')
 
 @section('head')
-<title>Iglesias</title>
+<title>Panel de control</title>
 @endsection
 
 @section('content')
@@ -19,7 +19,7 @@
                         <div>
                             <div class="flex justify-between flex-wrap items-center mb-6">
                                 <h4 class="font-medium lg:text-2xl text-xl capitalize text-slate-900 inline-block ltr:pr-4 rtl:pl-4 mb-4 sm:mb-0 flex space-x-3 rtl:space-x-reverse">
-                                    Dashboard</h4>
+                                    Panel de control</h4>
                                 <div class="flex sm:space-x-4 space-x-2 sm:justify-end items-center rtl:space-x-reverse">
                                     <button class="btn leading-0 inline-flex justify-center bg-white text-slate-700 dark:bg-slate-800 dark:text-slate-300 !font-normal">
                                         <span class="flex items-center">
